690b6c823b55bf3c01fe481cc801b79b0cb92c93
mateus <matheushenrique8790@outlook.com>
2025-05-21 19:46:04 -0300

    atualização Projeto java da plataforma dio.me

--- src/main/java/com/estudodiojava/ContaTerminal.java
+++ src/main/java/com/estudodiojava/ContaTerminal.java
@@ -4,5 +4,14 @@ package com.estudodiojava;
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
 
+        int saldo = 25;
+        int valorSolicitado = 18;
+
+        if (saldo >= valorSolicitado) {
+            saldo = saldo - valorSolicitado;
+        } else {
+
+        }
+        System.out.println(saldo);
     }
 }
